acd995ae17e9c776df6035ebbe5777e699069af7
Mitchell <mitchell.alton@live.com>
2020-05-07 05:02:14 -0500

    finished reward/chore forms

--- backend/app/controllers/chores_controller.rb
+++ backend/app/controllers/chores_controller.rb
@@ -1,9 +1,20 @@
 class ChoresController < ApplicationController
     
+    skip_before_action :verify_authenticity_token
     
     def index
         @chores = Chore.all 
         render json: @chores
     end
+
+    def create
+        @chore  = Chore.create(chore_params)
+    end
     
+    private
+
+    def chore_params
+        params.permit(:name, :pay, :family_id)
+    end
+
 end
